7d5191cbad05f15611bb0ec2688e7e3d1c15441f
Itchs <75327059+Itchss@users.noreply.github.com>
2021-01-11 13:40:11 -0800

    Added home page + resource pack download in GUI gen

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+// onClick functions
+function clickGUI() {
+ window.location.replace("gui.html");
+}
+
+function getContactInfo() {
+ alert("You can contact me on discord at: Itchs#7216. My DMs are open to anyone (just don't be a jerk)");
+}
