3c4666e33531c610ae590b405270605dfafa02a4
Itchs <75327059+Itchss@users.noreply.github.com>
2021-01-12 11:11:44 -0800

    Small QOL Changes, added gitignore

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
 
 // onClick functions
 function clickGUI() {
+
  window.location.replace("gui.html");
+
 }
 
 function getContactInfo() {
+
  alert("You can contact me on discord at: Itchs#7216. My DMs are open to anyone (just don't be a jerk)");
+
 }
+
+function signUp() {
+
+  window.location.replace("signup.html");
+
+}
+
+function login() {
+
+  window.location.replace("login.html");
+
+}
+
+// Modal stuff
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
