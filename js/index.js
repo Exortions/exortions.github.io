31593db9edfc4a4111938565ae38ab8dfca9f902
Itchs <75327059+Itchss@users.noreply.github.com>
2021-01-15 14:49:20 -0800

    Added some finishing touches to gui.html, and added a drop crafting generator.

--- js/index.js
+++ js/index.js
@@ -6,6 +6,12 @@ function clickGUI() {
 
 }
 
+function dropCrafting() {
+
+  window.location.replace("dropcrafting.html");
+ 
+}
+
 function getContactInfo() {
 
  alert("You can contact me on discord at: Itchs#7216. My DMs are open to anyone (just don't be a jerk)");
